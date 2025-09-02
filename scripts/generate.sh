64c6af016b6b8aa452df98699546aae3af524d8e
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2025-09-02 22:24:06 +0200

    Add CI

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,12 +2,14 @@
 
 rm -rf transpaer_api
 
+version=$(cargo get workspace.package.version)
+
 openapi-generator validate -i openapi/transpaer.json
 
 openapi-generator generate \
      -i openapi/transpaer.json \
      -o transpaer_api \
-     --additional-properties packageName=transpaer-api,packageVersion=0.3.0 \
+     --additional-properties packageName=transpaer-api,packageVersion=$version \
      -g rust-server
 
 cargo typify openapi/schemas.json -a PartialEq -o transpaer_api/src/models.rs
